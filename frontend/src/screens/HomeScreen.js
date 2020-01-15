68b9f26fa6f50004a119ca7870e29f1b8e5c8886
nodejsexpert76 <bealsthomas28@gmail.com>
2020-01-15 18:05:05 +0330

    api key

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,7 +5,9 @@ import LoadingBox from '../components/LoadingBox';
 import ErrorBox from '../components/ErrorBox';
 import { listProducts } from '../actions/productActions';
 
-const mongodbUrl = process.env.MONGODB_URL || 'mongodb://localhost/amazona';
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+console.log(API_KEY);
+const mongodbUrl = process.env.REACT_APP_WEATHER_API_KEY || 'mongodb://localhost/amazona';
 console.log(mongodbUrl);
 function HomeScreen(props) {
   const category = props.match.params.id;
