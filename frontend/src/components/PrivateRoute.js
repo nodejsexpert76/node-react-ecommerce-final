017d61a44b1b692cd7124ad643b3172ebe1a325e
nodejsexpert76 <bealsthomas28@gmail.com>
2020-01-25 08:48:55 +0330

    update PrivateRoute

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({ component, path, exact }) => (window.isAuth
-  ? (
-    <Route
-      path={path}
-      component={component}
-      {...exact}
-    />
-  )
-  : <Redirect to="/signin" />);
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) => (
+      window.isAuth === true
+        ? <Component {...props} />
+        : <Redirect to="/signin" />
+    )}
+  />
+);
+
 export default PrivateRoute;
